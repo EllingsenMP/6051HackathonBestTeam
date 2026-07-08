d253da4418eb8d06f810b466f4bb25cb69267a16
Cole Whittington <crwhitt@bu.edu>
2026-07-08 14:00:53 -0400

    Edits

--- scripts/codeSampling_Bulgarian.py
+++ scripts/codeSampling_Bulgarian.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('../data/eduBotTranslated - Bulgarian.csv')
+data = pd.read_excel('../data/eduBotTranslated - Bulgarian.xlsx')
 
 sample_100 = data.sample(100, random_state = 6051)
-sample_100.to_csv('../data/eduBotSampleBulgarian100.csv')
+sample_100.to_excel('../data/eduBotSampleBulgarian100.xlsx')
 
 sample_30 = data.sample(30, random_state = 6051)
-sample_30.to_csv('../data/eduBotSampleBulgarian30.csv')
+sample_30.to_excel('../data/eduBotSampleBulgarian30.xlsx')
